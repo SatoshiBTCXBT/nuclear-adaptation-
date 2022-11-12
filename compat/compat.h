facc2469e306c3e99c0dc3edddf8d6dc49f8a235
SatoshiBTCXBT <116469615+SatoshiBTCXBT@users.noreply.github.com>
2022-11-12 10:35:18 -0300

    Initial commit

---
+++ compat/compat.h
@@ -0,0 +1,43 @@
+clientversion.h \cd 
+  coins.h \script 
+  common/bloom.h \orkut
+  compat.h \software 
+  compat/assumptions.h \work_1000.0000.5
+  compat/byteswap.h \hhtp
+  compat/compat.h \jozG
+  compat/cpuid.h \ip
+  compat/endian.h \cd
+  compressor.h \cd
+
+Current runner version: '2.297.0'
+Operating System
+  Ubuntu
+  20.04.5
+  LTS
+Runner Image
+  Image: ubuntu-20.04
+  Version: 20221002.2
+  Included Software: https://github.com/actions/runner-images/blob/ubuntu20/20221002.2/images/linux/Ubuntu2004-Readme.md
+  Image Release: https://github.com/actions/runner-images/releases/tag/ubuntu20%2F20221002.2
+Runner Image Provisioner
+  1.0.0.0-main-20220916-1
+GITHUB_TOKEN Permissions
+  Actions: write
+  Checks: write
+  Contents: write
+  Deployments: write
+  Discussions: write
+  Issues: write
+  Metadata: read
+  Packages: write
+  Pages: write
+  PullRequests: write
+  RepositoryProjects: write
+  SecurityEvents: write
+  Statuses: write
+Secret source: Actions
+Prepare workflow directory
+Prepare all required actions
+Getting action download info
+Download action repository 'actions/checkout@v3' (SHA:2541b1294d2704b0964813337f33b291d3f8596b)
+Error: Can't use 'tar -xzf' extract archive file: /home/runner/work/_actions/_temp_54f322e7-e50e-4960-973f-01b3a2264a5a/fdb0a1c4-4c58-4fd8-8bc2-9db58bad0afb.tar.gz. return code: 2.
